22123fc23270791e6c666a9d2f453a4ed634d6f4
Marcos-Barry <55248963+Marcos-Barry@users.noreply.github.com>
2020-05-21 08:12:04 -0300

    atualizando

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -21,11 +22,34 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();//verificando se o usuario esta logado
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
     
 	
+});
+
+$app->get('/admin/login',function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+	
+
+	User::login($_POST["login"],$_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
 });
 
 $app->run();
