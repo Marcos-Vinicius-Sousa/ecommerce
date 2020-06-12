9c1e7a134f9285ddbf9b55d537bd64893a34c9f4
dev-barry <rosario.marcosv@gmail.com>
2020-06-11 23:19:35 -0300

    começando o crud do usuario
    comando listAll ainda nao esta trazendo a lista de usuarios

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -38,9 +38,9 @@ class User extends Model {
 		}
 	}
 
-	public static function verifyLogin($inadmin = 0){
+	public static function verifyLogin($inadmin = true){
 
-		var_dump($_SESSION);
+		
 		if(
 			!isset($_SESSION[User::SESSION])// verificando se a sessao esta definida  
 			||
@@ -56,6 +56,37 @@ class User extends Model {
 		}
 
 	}
+
+	public static function logout(){
+
+		$_SESSION[User::SESSION] = NULL;
+		exit;
+	}
+
+	public static function listAll(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users  a inner join tb_persons b using(idperson) order by b.desperson");
+	}
+
+	public function save(){
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson , :deslogin , :despassword , :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this-> setData($results[0]);
+	}
+
+
 
 }
 
